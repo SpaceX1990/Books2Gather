eaaca83766fd0d2df906422c61bf8d7d468c46ba
dua <dua@neusta.de>
2025-02-21 10:17:12 +0100

    refactor autor class for mapping to database

--- Models/Author.cs
+++ Models/Author.cs
@@ -1,9 +1,38 @@
-﻿namespace Books2Gather.Models {
-    public class Author {
-        public string Firstname { get; set; }
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
-        public string Lastname { get; set; }
+namespace Books2Gather.Models
+{
+    [Table("Autoren")] // Entspricht der SQL-Tabelle
+    public class Author
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("AutorID")] // Entspricht der SQL-Spalte
+        public int AuthorId { get; set; }
 
-        public DateOnly Birthday { get; set; }
+        [Required]
+        [MaxLength(50)]
+        [Column("Vorname")] // Deutsche Spaltenbezeichnung
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        [Column("Nachname")]
+        public string LastName { get; set; }
+
+        [Column("Geburtsdatum")] public DateTime? BirthDate { get; set; }
+
+        [MaxLength(50)]
+        [Column("Nationalitaet")]
+        public string Nationality { get; set; }
+
+        [Column("Biografie")] public string Biography { get; set; }
+
+        [NotMapped] // Diese Eigenschaft wird NICHT in die Datenbank geschrieben
+        public string FullName
+        {
+            get { return $"{FirstName} {LastName}"; }
+        }
     }
 }
