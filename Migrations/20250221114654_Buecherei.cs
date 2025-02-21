a6d3b384ce3357d40f379b6094999ea87e07df89
VTischer <viktor.t1@schule.bremen.de>
2025-02-21 12:49:04 +0100

    Migrationsscript und Datenbankerstellung

---
+++ Migrations/20250221114654_Buecherei.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Books2Gather.Migrations
+{
+    /// <inheritdoc />
+    public partial class Buecherei : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Autoren",
+                columns: table => new
+                {
+                    AutorID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Vorname = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Nachname = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Geburtsdatum = table.Column<DateOnly>(type: "date", nullable: true),
+                    Nationalitaet = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Biografie = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Autoren", x => x.AutorID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Buecher",
+                columns: table => new
+                {
+                    BuchID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Titel = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    ISBN = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    Erscheinungsdatum = table.Column<DateOnly>(type: "date", nullable: false),
+                    Preis = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Buecher", x => x.BuchID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Genres",
+                columns: table => new
+                {
+                    GenreID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Beschreibung = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Genres", x => x.GenreID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AuthorBook",
+                columns: table => new
+                {
+                    AuthorsAuthorId = table.Column<int>(type: "int", nullable: false),
+                    BooksBookId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AuthorBook", x => new { x.AuthorsAuthorId, x.BooksBookId });
+                    table.ForeignKey(
+                        name: "FK_AuthorBook_Autoren_AuthorsAuthorId",
+                        column: x => x.AuthorsAuthorId,
+                        principalTable: "Autoren",
+                        principalColumn: "AutorID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AuthorBook_Buecher_BooksBookId",
+                        column: x => x.BooksBookId,
+                        principalTable: "Buecher",
+                        principalColumn: "BuchID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BookGenre",
+                columns: table => new
+                {
+                    BooksBookId = table.Column<int>(type: "int", nullable: false),
+                    GenresGenreId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BookGenre", x => new { x.BooksBookId, x.GenresGenreId });
+                    table.ForeignKey(
+                        name: "FK_BookGenre_Buecher_BooksBookId",
+                        column: x => x.BooksBookId,
+                        principalTable: "Buecher",
+                        principalColumn: "BuchID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BookGenre_Genres_GenresGenreId",
+                        column: x => x.GenresGenreId,
+                        principalTable: "Genres",
+                        principalColumn: "GenreID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AuthorBook_BooksBookId",
+                table: "AuthorBook",
+                column: "BooksBookId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookGenre_GenresGenreId",
+                table: "BookGenre",
+                column: "GenresGenreId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AuthorBook");
+
+            migrationBuilder.DropTable(
+                name: "BookGenre");
+
+            migrationBuilder.DropTable(
+                name: "Autoren");
+
+            migrationBuilder.DropTable(
+                name: "Buecher");
+
+            migrationBuilder.DropTable(
+                name: "Genres");
+        }
+    }
+}
