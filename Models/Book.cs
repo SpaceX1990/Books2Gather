c392c140254202b6fe8dbecec6304f140a592977
VTischer <viktor.t1@schule.bremen.de>
2025-02-21 10:15:47 +0100

    Gui mit testdaten und Filterung

--- Models/Book.cs
+++ Models/Book.cs
@@ -1,15 +1,16 @@
-﻿namespace Books2Gather.Models {
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Books2Gather.Models {
     public class Book {
         public string Title { get; set; }
-
-        public List<Author> AuthorList { get; set; }
-
-        public List<Genre> GenreList { get; set; }
-
+        public List<Author> AuthorList { get; set; } = new List<Author>();
+        public List<Genre> GenreList { get; set; } = new List<Genre>();
         public string ISBN { get; set; }
-
         public DateOnly PublishingDate { get; set; }
-
-        public Decimal Prize { get; set; }
+        public decimal Prize { get; set; }
+        public string AuthorNames => string.Join(", ", AuthorList.Select(a => $"{a.Firstname} {a.Lastname}"));
+        public string GenreNames => string.Join(", ", GenreList.Select(g => g.Description));
     }
 }
--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -1,34 +1,94 @@
 ﻿using Books2Gather.Models;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Windows.Data;
 using System.Windows.Input;
-using System.Windows;
-using CommunityToolkit.Mvvm.Input;
 
 namespace Books2Gather.ViewModels {
-    public class MainViewModel {
+    public class MainViewModel : INotifyPropertyChanged {
         public ObservableCollection<Book> Books { get; set; }
+        public ICollectionView FilteredBooks { get; }
+
+        private string _searchQuery;
+        public string SearchQuery {
+            get => _searchQuery;
+            set {
+                _searchQuery = value;
+                OnPropertyChanged(nameof(SearchQuery));
+                FilteredBooks.Refresh();
+            }
+        }
+
         public ICommand EditBookCommand { get; }
         public ICommand DeleteBookCommand { get; }
 
         public MainViewModel() {
             Books = new ObservableCollection<Book>
             {
-            new Book { Title = "Der Hobbit", ISBN = "123456789"},
-            new Book { Title = "1984", ISBN = "987654321"}
-        };
+                new Book
+                {
+                    Title = "Der Hobbit",
+                    ISBN = "123456789",
+                    AuthorList = new List<Author>
+                    {
+                        new Author { Firstname = "J.R.R.", Lastname = "Tolkien", Birthday = new DateOnly(1892, 1, 3) }
+                    },
+                    GenreList = new List<Genre>
+                    {
+                        new Genre { Description = "Fantasy" }
+                    },
+                    PublishingDate = new DateOnly(1937, 9, 21),
+                    Prize = 12.99m
+                },
+                new Book
+                {
+                    Title = "1984",
+                    ISBN = "987654321",
+                    AuthorList = new List<Author>
+                    {
+                        new Author { Firstname = "George", Lastname = "Orwell", Birthday = new DateOnly(1903, 6, 25) }
+                    },
+                    GenreList = new List<Genre>
+                    {
+                        new Genre { Description = "Dystopie" }
+                    },
+                    PublishingDate = new DateOnly(1949, 6, 8),
+                    Prize = 9.99m
+                }
+            };
+
+            FilteredBooks = CollectionViewSource.GetDefaultView(Books);
+            FilteredBooks.Filter = FilterBooks;
 
             EditBookCommand = new RelayCommand<Book>(EditBook);
             DeleteBookCommand = new RelayCommand<Book>(DeleteBook);
         }
 
+        private bool FilterBooks(object item) {
+            if (item is Book book) {
+                return string.IsNullOrEmpty(SearchQuery) ||
+                       book.Title.Contains(SearchQuery, StringComparison.OrdinalIgnoreCase) ||
+                       book.ISBN.Contains(SearchQuery, StringComparison.OrdinalIgnoreCase) ||
+                       book.AuthorList.Any(a =>
+                           a.Firstname.Contains(SearchQuery, StringComparison.OrdinalIgnoreCase) ||
+                           a.Lastname.Contains(SearchQuery, StringComparison.OrdinalIgnoreCase) ||
+                           $"{a.Firstname} {a.Lastname}".Contains(SearchQuery, StringComparison.OrdinalIgnoreCase)) ||
+                       book.GenreList.Any(g => g.Description.Contains(SearchQuery, StringComparison.OrdinalIgnoreCase));
+            }
+            return false;
+        }
+
         private void EditBook(Book book) {
-            MessageBox.Show($"Bearbeiten: {book.Title}");
+            throw new NotImplementedException();
         }
 
         private void DeleteBook(Book book) {
-            MessageBox.Show($"Löschen: {book.Title}");
+            throw new NotImplementedException();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
-
 }
