a693a89a46d493c738da82ccc60f933bf26e05c0
dua <dua@neusta.de>
2025-03-07 10:45:25 +0100

    add migration

---
+++ Migrations/20250307092856_ucherei2.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Books2Gather.Migrations
+{
+    /// <inheritdoc />
+    public partial class ucherei2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "PublishingDate",
+                table: "Books",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "BookId",
+                keyValue: 1,
+                column: "PublishingDate",
+                value: new DateTime(1954, 7, 29, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "BookId",
+                keyValue: 2,
+                column: "PublishingDate",
+                value: new DateTime(1997, 6, 26, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "BookId",
+                keyValue: 3,
+                column: "PublishingDate",
+                value: new DateTime(1937, 9, 21, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "PublishingDate",
+                table: "Books",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "BookId",
+                keyValue: 1,
+                column: "PublishingDate",
+                value: new DateOnly(1954, 7, 29));
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "BookId",
+                keyValue: 2,
+                column: "PublishingDate",
+                value: new DateOnly(1997, 6, 26));
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "BookId",
+                keyValue: 3,
+                column: "PublishingDate",
+                value: new DateOnly(1937, 9, 21));
+        }
+    }
+}
